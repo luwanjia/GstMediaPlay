3b3f350eca9939990a78bbee2fb3fb7e3ee52328
luwanjia <luwanjia@aliyun.com>
2017-09-20 18:21:52 +0800

    Pass video file by command.

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,23 @@
-#include <gst/gst.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <gst/gst.h>
 #include "gst_player.h"
 
 int main(int argc, char *argv[]) {
-  GstPlayer mediaplayer("./test_video.mp4");
+    std::string filename;
+    
+    if (argc < 2) {
+        printf("Error: Please input video file name.\n");
+        return 0;
+    }
+    
+    filename = argv[1];
+    printf("-- Playing: %s\n", filename.c_str());
+    GstPlayer mediaplayer(filename);
   
-  mediaplayer.play();
-  return 0;
+    mediaplayer.play();
+    
+    printf("-- Exit.\n");
+    return 0;
 }
